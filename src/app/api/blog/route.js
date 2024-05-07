3fdba0a7b0203cf07fb80a40e14746fc2bd9b8c6
dannymhmdi <dannymohammadi1377@gmail.com>
2024-05-07 21:23:01 +0330

    process on fetching data on api

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -1,17 +1,17 @@
-import connectToDb from "@/lib/connectTodb"
-import { NextResponse } from "next/server"
+import connectToDb from "@/lib/connectTodb";
+import { NextResponse } from "next/server";
 
-export const GET = async (req , res) =>{
-try{
- const response  = await connectToDb(`select * from cars where brand='bentley'`)
-     return NextResponse.json(response)
-}
-catch(err){
-console.log('error in server',err)
-// throw new Error('error in connection to database occured')
-}
-
-
-
-
-}
+export const GET = async (req, res) => {
+  try {
+    const response = await connectToDb(
+      `select * from cars where brand='bentley'`
+    );
+    const newRes = response?.map((value) => {
+      return { name: value?.brand };
+    });
+    return NextResponse.json(newRes);
+  } catch (err) {
+    console.log("error in server", err);
+    // throw new Error('error in connection to database occured')
+  }
+};
